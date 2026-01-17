c10525556fd1589e826db2f0c0d880eb33b6eae0
Arda Aydın <120125109+aydinarda@users.noreply.github.com>
2026-01-17 14:59:52 +0100

    Add files via upload

--- optimize/Scenario_Setting_For_SC2F.py
+++ optimize/Scenario_Setting_For_SC2F.py
@@ -181,7 +181,7 @@ def run_scenario(
         for loc in new_loc_openingCost
     }
     
-    new_loc_unitCost = {loc: (1 / cap) * 100000 for loc, cap in new_loc_capacity.items()}
+    new_loc_unitCost = {loc: (1 / cap) * 90000 for loc, cap in new_loc_capacity.items()}
 
     # -----------------------------
     # DISTANCES (in km)
